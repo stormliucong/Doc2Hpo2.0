2ebb50909752967a2bfbefbf61122d752f39ac1e
stormliucong <stormliucong@gmail.com>
2024-12-11 14:55:18 -0500

    update autocomplete

--- front-end/src/Components/SearchDialog.js
+++ front-end/src/Components/SearchDialog.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import {
     Dialog,
     DialogTitle,
@@ -16,11 +16,16 @@ import {
 
 } from '@mui/material';
 import { v4 as uuidv4 } from 'uuid';
+import { AppContext } from './AppContext';
+import Autocomplete from '@mui/material/Autocomplete';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const SearchDialog = ({ open, onClose, onConfirm, selectedHighlight }) => {
     const [searchQuery, setSearchQuery] = useState('');
     const [suggestions, setSuggestions] = useState([]);
     const [selectedItem, setSelectedItem] = useState(null);
+    const { loading, setLoading, error, setError } = useContext(AppContext);
+
 
 
     useEffect(() => {
@@ -29,24 +34,56 @@ const SearchDialog = ({ open, onClose, onConfirm, selectedHighlight }) => {
         }
     }, [selectedHighlight]);
 
+    // useEffect(() => {
+    //     if (searchQuery) {
+    //         setLoading(true);
+    //         // https://clinicaltables.nlm.nih.gov/apidoc/hpo/v3/doc.html
+    //         fetch(`https://clinicaltables.nlm.nih.gov/api/hpo/v3/search?terms=${searchQuery}`)
+    //             .then((res) => res.json())
+    //             .then((data) => {
+    //                 // reformating the data to match the expected format
+    //                 const hpoIdNameList = data[3]
+    //                 const hpoIdNameFormatList = hpoIdNameList.map((hpoIdName, index) => ({ id: hpoIdName[0], name: hpoIdName[1] }));
+    //                 setSuggestions(hpoIdNameFormatList);
+    //             })
+    //             .catch((err) => {
+    //                 console.error('Error fetching suggestions:', err);
+    //                 setSuggestions([]);
+    //             });
+    //     } else {
+    //         setSuggestions([]);
+    //         return;
+    //     }
+    // }, [searchQuery]);
+
     useEffect(() => {
-        if (searchQuery) {
-            // https://clinicaltables.nlm.nih.gov/apidoc/hpo/v3/doc.html
-            fetch(`https://clinicaltables.nlm.nih.gov/api/hpo/v3/search?terms=${searchQuery}`)
-                .then((res) => res.json())
-                .then((data) => {
-                    // reformating the data to match the expected format
-                    const hpoIdNameList = data[3]
-                    const hpoIdNameFormatList = hpoIdNameList.map((hpoIdName, index) => ({ id: hpoIdName[0], name: hpoIdName[1] }));
-                    setSuggestions(hpoIdNameFormatList);
-                })
-                .catch((err) => {
-                    console.error('Error fetching suggestions:', err);
-                    setSuggestions([]);
-                });
-        } else {
+        if (!searchQuery) {
             setSuggestions([]);
         }
+
+        const fetchOptions = async () => {
+            setLoading(true);
+            try {
+                // Replace with your actual API call
+                const response = await fetch(`https://clinicaltables.nlm.nih.gov/api/hpo/v3/search?terms=${searchQuery}`);
+                const data = await response.json();
+
+                const hpoIdNameList = data[3]
+                const hpoIdNameFormatList = hpoIdNameList.map((hpoIdName, index) => ({ id: hpoIdName[0], name: hpoIdName[1] }));
+                // Assume the API returns an array of strings for simplicity
+                console.log(hpoIdNameFormatList)
+                setSuggestions(hpoIdNameFormatList || []);
+            } catch (error) {
+                console.error('Error fetching options:', error);
+                setError(error);
+                setSuggestions([]);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        const debounceTimeout = setTimeout(fetchOptions, 500); // Debounce the API call
+        return () => clearTimeout(debounceTimeout); // Cleanup the timeout
     }, [searchQuery]);
 
     const handleSelect = (item) => {
@@ -64,15 +101,67 @@ const SearchDialog = ({ open, onClose, onConfirm, selectedHighlight }) => {
         <Dialog open={open} onClose={onClose} fullWidth maxWidth="sm">
             <DialogTitle>Search</DialogTitle>
             <DialogContent>
+                <Autocomplete
+                    freeSolo // Allows custom input (not limited to options)
+                    options={(suggestions)} // Display the name property
+                    getOptionLabel={(option) => (option.name ? option.name : '')} // Display the name property
+                    isOptionEqualToValue={(option, value) => option.id === value.id} // Match by id
+                    loading={loading}
+                    inputValue={searchQuery}
+                    onInputChange={(event, newInputValue) => {
+                        setSearchQuery(newInputValue);
+                    }}
+                    onChange={(event, newValue) => {
+                        setSelectedItem(newValue);
+                    }}
+                    renderOption={(props, option) => (
+                        <Box
+                          component="li"
+                          {...props}
+                          sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            padding: '4px 8px',
+                          }}
+                        >
+                          <Typography variant="body1">{option.name}</Typography>
+                          <Typography variant="body2" color="text.secondary" sx={{ marginLeft: 1 }}>
+                            ({option.id})
+                          </Typography>
+                        </Box>
+                      )}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="Search"
+                            variant="outlined"
+                            InputProps={{
+                                ...params.InputProps,
+                                endAdornment: (
+                                    <>
+                                        {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                                        {params.InputProps.endAdornment}
+                                    </>
+                                ),
+                            }}
+                        />
+                    )}
+                />
+                {/* Information Card Section
+                <Box display="flex" alignItems="center">
+                </Box> */}
 
-                <Box display="flex" alignItems="center"><TextField
+                {/* <Box display="flex" alignItems="center"><TextField
                     fullWidth
                     label="Search"
                     variant="outlined"
                     value={searchQuery}
                     onChange={(e) => setSearchQuery(e.target.value)}
                 />
-                {suggestions &&    <List>
+                </Box> */}
+                {/* <Box display="flex" alignItems="center" fullWidth>
+                    {suggestions && <List>
                         {suggestions.map((item) => (
                             <ListItem
                                 key={uuidv4()}
@@ -83,30 +172,10 @@ const SearchDialog = ({ open, onClose, onConfirm, selectedHighlight }) => {
                                 <ListItemText primary={item.id} secondary={item.name} />
                             </ListItem>
                         ))}
-                    </List> 
-                }
-                {suggestions.length === 0 && <Typography>No suggestions found. Change the search query</Typography>}
-                </Box>
-                <Box mt={2}>
-                    {/* Information Card Section */}
-                    {selectedItem && (
-                        <Card sx={{ mt: 3 }}>
-                            <CardContent>
-                                <Typography variant="h6">Selected Item</Typography>
-                                <Typography variant="body1">HPO ID: {selectedItem.id}</Typography>
-                                <Typography variant="body1">HPO Name: {selectedItem.name}</Typography>
-                            </CardContent>
-                        </Card>
-                    )}
-                    {!selectedItem && (
-                        <Card sx={{ mt: 3 }}>
-                            <CardContent>
-                                <Typography variant="h6">No item selected</Typography>
-                            </CardContent>
-                        </Card>
-                    )}
-                </Box>
-
+                    </List>
+                    }
+                    {suggestions.length === 0 && <Typography>No suggestions found. Change the search query</Typography>}
+                </Box> */}
             </DialogContent>
             <DialogActions>
                 <Button onClick={onClose}>Cancel</Button>
