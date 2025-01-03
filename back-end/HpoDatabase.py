a8d281edce24a4c1e71023915a772776002c7a12
stormliucong <stormliucong@gmail.com>
2025-01-02 22:45:54 -0500

    add whoosh and top 5 evaluation

--- back-end/HpoDatabase.py
+++ back-end/HpoDatabase.py
@@ -15,14 +15,19 @@ from collections import defaultdict
 from typing import List
 from HpoFactory import HpoFactory
 import chromadb
+from whoosh import index
+from whoosh.fields import Schema, TEXT, ID
+from whoosh.qparser import QueryParser
+
 
 
 # Define the HPO database class
 class HpoDatabase:
-    def __init__(self, db_path, obo_path):
+    def __init__(self, db_path, obo_path, woosh_path=None):
         
         self.chromadb_client = chromadb.PersistentClient(path=db_path)
         self.obo_path = obo_path
+        self.whoosh_path = woosh_path
         
     def parse_obo(self, obo_path):
         with open(obo_path, 'r') as f:
@@ -60,59 +65,138 @@ class HpoDatabase:
                 defition = line.split('"')[1]
         return hpo_object_list
     
-    def index_hpo(self):
-        
-        # create collection
-        collection = self.chromadb_client.get_or_create_collection(name='hpo_default')
-        
+    def index_hpo(self, use_chromdb=True):
         hpo_object_list = self.parse_obo(self.obo_path)
-        # do batch insert for every 100 documents
-        i = 0
-        while i < len(hpo_object_list):
-            current_batch = hpo_object_list[i:i+100] if i+100 < len(hpo_object_list) else hpo_object_list[i:]
-            documents = [b['document'] for b in current_batch]
-            metadatas = [b['metadata'] for b in current_batch]
-            ids = [b['id'] for b in current_batch]
-            collection.upsert(
-                documents=documents,
-                metadatas=metadatas,
-                ids=ids
-            )
-            i += 100
-            print(f"Indexed {i} documents.")
-            
-    def query_hpo(self, text, n_results=1):
         
-        # get collection
-        collection = self.chromadb_client.get_collection(name='hpo_default')
+        if use_chromdb:
         
-        # query
-        results = collection.query(
-            query_texts=[text],
-            n_results=n_results,
-            
-        )
-        return results
-    
-    def parse_results(self, results, distance_threshold = 1.5):
-        # if more than one results raise error
-        if len(results['distances'][0]) > 1:
-            raise ValueError("More than one results found.")
-        if results['distances'][0][0] < distance_threshold:
-            hpo_id = results['ids'][0][0]
-            hpo_name = results['metadatas'][0][0]['term']
-            return hpo_id, hpo_name
+            # create collection
+            collection = self.chromadb_client.get_or_create_collection(name='hpo_default')
+
+            # do batch insert for every 100 documents
+            i = 0
+            while i < len(hpo_object_list):
+                current_batch = hpo_object_list[i:i+100] if i+100 < len(hpo_object_list) else hpo_object_list[i:]
+                documents = [b['document'] for b in current_batch]
+                metadatas = [b['metadata'] for b in current_batch]
+                ids = [b['id'] for b in current_batch]
+                collection.upsert(
+                    documents=documents,
+                    metadatas=metadatas,
+                    ids=ids
+                )
+                i += 100
+                print(f"Indexed {i} documents.")
         else:
-            return None, None
+            print("Use Woosh")
+            # Define the schema
+            schema = Schema(id=ID(stored=True, unique=True), term=TEXT(stored=True), content=TEXT(stored=True))
+            index_dir = self.whoosh_path
+            # Create the index directory if it doesn't exist
+            if not os.path.exists(index_dir):
+                os.mkdir(index_dir)
+                ix = index.create_in(index_dir, schema)  # Create the index in the directory
+            else:
+                ix = index.open_dir(index_dir)  # Open the existing index
+
+            # Add documents to the index
+            
+            i = 0
+            while i < len(hpo_object_list):
+                writer = ix.writer()
+                current_batch = hpo_object_list[i:i+100] if i+100 < len(hpo_object_list) else hpo_object_list[i:]
+                documents = [b['document'] for b in current_batch]
+                metadatas = [b['metadata'] for b in current_batch]
+                for object in current_batch:
+                    writer.add_document(id=object['id'], term=object['metadata']['term'], content=object['document'])
+                writer.commit()
+                i += 100
+                print(f"Indexed {i} documents.")
+                
+            
+    def query_hpo(self, text, n_results=1, use_chromdb=True):
+        
+        if use_chromdb:
+            
+        
+            # get collection
+            collection = self.chromadb_client.get_collection(name='hpo_default')
+            
+            # query
+            results = collection.query(
+                query_texts=[text],
+                n_results=n_results,
+                
+            )
+            return results
+        else:
+            print("Use Woosh")
+            ix = index.open_dir(self.whoosh_path)
+            with ix.searcher() as searcher:
+                # Search in the title field
+                term_query = QueryParser("term", ix.schema).parse(text)
+                term_results = searcher.search(term_query)
+                if not term_results.is_empty():
+                    results = term_results
+                else:
+                    content_query = QueryParser("content", ix.schema).parse(text)
+                    results = searcher.search(content_query)
+                    if results.is_empty():
+                        # query with 'OR' operator
+                        terms = text.split()
+                        query = " OR ".join(terms)
+                        query = QueryParser("term", ix.schema).parse(query)  
+                        results = searcher.search(query)
+                if results.is_empty():
+                    return (None, None)
+                else:
+                    number_of_results = min(n_results, len(results.top_n))                    
+                    result_list = []
+                    for i in range(number_of_results):
+                        hpo_id = results[i]['id']
+                        hpo_name = results[i]['term']
+                        score = results[i].score
+                        result_list.append((hpo_id, hpo_name, score))
+                    return result_list
+                    
+
+    def parse_results(self, results, distance_threshold = 1.5, use_chromdb=True):
+        # if more than one results raise error
+        if use_chromdb:
+            if len(results['distances'][0]) > 1:
+                raise ValueError("More than one results found.")
+            if results['distances'][0][0] < distance_threshold:
+                hpo_id = results['ids'][0][0]
+                hpo_name = results['metadatas'][0][0]['term']
+                return hpo_id, hpo_name
+            else:
+                return None, None
+        else:
+            if results[0] is None:
+                return None, None
+            else:
+                return results[0][0], results[0][1]
+            
     
-    def parse_results_n_results(self, results):
-        parsed_n_results = []
-        for top_k in range(len(results['distances'][0])):
-            hpo_id = results['ids'][0][top_k]
-            hpo_name = results['metadatas'][0][top_k]['term']
-            distance = results['distances'][0][top_k]
-            parsed_n_results.append({'hpo_id': hpo_id, 'hpo_name': hpo_name, 'top_k': top_k, 'distance': distance})
-        return parsed_n_results 
+    def parse_results_n_results(self, results, use_chromdb=True):
+        if use_chromdb:
+            parsed_n_results = []
+            for top_k in range(len(results['distances'][0])):
+                hpo_id = results['ids'][0][top_k]
+                hpo_name = results['metadatas'][0][top_k]['term']
+                distance = results['distances'][0][top_k]
+                parsed_n_results.append({'hpo_id': hpo_id, 'hpo_name': hpo_name, 'top_k': top_k, 'distance': distance})
+            return parsed_n_results 
+        else:
+            parsed_n_results = []
+            if results[0] is None:
+                return parsed_n_results
+            for top_k in range(len(results)):
+                hpo_id = results[top_k][0]
+                hpo_name = results[top_k][1]
+                distance = -results[top_k][2]
+                parsed_n_results.append({'hpo_id': hpo_id, 'hpo_name': hpo_name, 'top_k': top_k, 'distance': distance})
+            return parsed_n_results
            
             
 
@@ -120,16 +204,21 @@ class HpoDatabase:
 if __name__ == "__main__":
     db_path = 'hpo_chroma_db'
     obo_path = 'hp.obo'
+    whoosh_path = 'hpo_whoosh_db'
     
-    hpo_db = HpoDatabase(db_path, obo_path)
+    hpo_db = HpoDatabase(db_path, obo_path,whoosh_path)
     # chroma run --path ./hpo_chroma_db
     # hpo_object_list = hpo_db.parse_obo(obo_path)
     # print(hpo_object_list[:5])
-    hpo_db.index_hpo()
-    # hpo_term = "Difficult to feed"
-    # results = hpo_db.query_hpo(hpo_term)
-    # hpo_id, hpo_name = hpo_db.parse_results(results)
-    # print(hpo_id, hpo_name)
+    # hpo_db.index_hpo(use_chromdb=False)
+    hpo_term = "feeding dsad"
+    results = hpo_db.query_hpo(hpo_term, n_results=1, use_chromdb=False)
+    hpo_id, hpo_name = hpo_db.parse_results(results, use_chromdb=False)
+    print(hpo_id, hpo_name)
+    
+    results = hpo_db.query_hpo(hpo_term, n_results=100, use_chromdb=False)
+    parsed_n_results = hpo_db.parse_results_n_results(results, use_chromdb=False)
+    print(parsed_n_results)
     
     
         
--- back-end/rag_evaluation/rag_evaluation_top1_whoosh.py
+++ back-end/rag_evaluation/rag_evaluation_top1_whoosh.py
@@ -8,7 +8,8 @@ import random
 
 db_path = '../hpo_chroma_db'
 obo_path = '../hp.obo'
-hpo_db = HpoDatabase(db_path, obo_path)
+whoosh_path = '../hpo_whoosh_db'
+hpo_db = HpoDatabase(db_path, obo_path,whoosh_path)
 # # test query
 # query = "muscle weakness"
 # results = hpo_db.query_hpo(query)
@@ -26,6 +27,7 @@ if os.path.exists(synonym_folder):
     for file in os.listdir(synonym_folder):
         with open(os.path.join(synonym_folder, file), "r") as f:
             expanded_hpo_name_dict.update(json.load(f))
+len(expanded_hpo_name_dict)
 print(f"There are {len(expanded_hpo_name_dict)} HPO terms with synonyms")
 
 
@@ -36,27 +38,26 @@ for hp_id in expanded_hpo_name_dict:
     i += 1
     if i % 1000 == 0:
         print(f"Processed {i} HPO terms")       
-    
-    # if i > 100:
-    #     break   
     query_results[hp_id] = []
     for synonym in expanded_hpo_name_dict[hp_id]:
-        # p = random.random()
         # create a random indicator p (0,1) to decide whether to query the synonym
-        # for testing purposes onlyl.
-        # takes ~5 minutes to query 500 HPO terms
-        # on average, it takes 0.6 seconds to query one HPO term
-        # so it will take 0.6 * 18000 / 3600 = 3 hours to query all HPO terms
+        # p = random.random()
         # if p > 0.01:
+        #     # takes ~5 minutes to query 500 HPO terms
+        #     # on average, it takes 0.6 seconds to query one HPO term
+        #     # so it will take 0.6 * 18000 / 3600 = 3 hours to query all HPO terms
         #     continue
-        results = hpo_db.query_hpo(synonym, n_results=5)
-        parsed_results = hpo_db.parse_results_n_results(results)
-        
-        query_results[hp_id].append({"synonym": synonym, "parsed_results": parsed_results})
+        results = hpo_db.query_hpo(synonym, use_chromdb=False)
+        hit_hpo_id, _ = hpo_db.parse_results(results, use_chromdb=False)
+        if hit_hpo_id:
+            query_results[hp_id].append(hit_hpo_id)
+        else:
+            print(f"No HPO id found for {synonym}")
+            query_results[hp_id].append(None)
 print(f"Finished querying {i} HPO terms")
 
 # write the query results to a json file
-with open('./rag_evaluation_query_results_top5_default.json', 'w') as f:
+with open('./rag_evaluation_query_results_top1_whoosh.json', 'w') as f:
     json.dump(query_results, f)
     
-print("Query results saved to rag_evaluation_query_results_top5_default.json")
+print("Query results saved to rag_evaluation_query_results_top1_whoosh.json")
